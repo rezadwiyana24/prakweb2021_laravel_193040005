b4d77fdad00de371868df5eb8162eb5961dc328b
rezadwiyana24 <rezadwiyana93@gmail.com>
2021-10-14 21:03:03 +0700

    Menyelesaikan Vidio 5

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -31,56 +34,6 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Reza Dwiyana",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime corrupti, praesentium iste unde magni quidem? Earum fugiat eos sint voluptatum consequatur natus, veritatis, ab adipisci, rem labore deserunt modi alias distinctio officiis voluptate at aliquam tempore sunt dignissimos molestias eveniet commodi quaerat aspernatur. Iste porro odit odio ad distinctio dolorem maiores sed dolores atque rerum repellendus iure voluptate dignissimos quia, molestias, modi, error ullam unde placeat quam praesentium? Consectetur optio repellendus enim delectus accusamus sequi in officiis itaque. Repellat, minus."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Ferdian Eka",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis nobis ab, labore maiores blanditiis impedit quaerat quis! Reiciendis eligendi non omnis illo iusto animi corrupti sapiente, doloribus reprehenderit nesciunt iure ex vero quisquam tempora. Vero et minus magnam omnis optio nemo dolor modi debitis officiis eaque libero magni, doloremque soluta eos. Molestiae quam vitae ex, id qui blanditiis atque temporibus nostrum dicta magni commodi laboriosam nobis eaque quasi magnam eveniet perspiciatis optio ullam! Facilis praesentium amet quas quae, deserunt neque eius reprehenderit natus est unde corporis, voluptatibus temporibus eaque at quasi ut vitae odio debitis nostrum repudiandae perferendis eum delectus."
-        ],
-    ];
-
-    return view('Posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
+Route::get('/blog', [PostController::class, 'index']);
 // halaman single post
-Route:: get('posts/{slug}', function($slug) {
-    $blog_posts = [
-
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Reza Dwiyana",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime corrupti, praesentium iste unde magni quidem? Earum fugiat eos sint voluptatum consequatur natus, veritatis, ab adipisci, rem labore deserunt modi alias distinctio officiis voluptate at aliquam tempore sunt dignissimos molestias eveniet commodi quaerat aspernatur. Iste porro odit odio ad distinctio dolorem maiores sed dolores atque rerum repellendus iure voluptate dignissimos quia, molestias, modi, error ullam unde placeat quam praesentium? Consectetur optio repellendus enim delectus accusamus sequi in officiis itaque. Repellat, minus."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Ferdian Eka",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis nobis ab, labore maiores blanditiis impedit quaerat quis! Reiciendis eligendi non omnis illo iusto animi corrupti sapiente, doloribus reprehenderit nesciunt iure ex vero quisquam tempora. Vero et minus magnam omnis optio nemo dolor modi debitis officiis eaque libero magni, doloremque soluta eos. Molestiae quam vitae ex, id qui blanditiis atque temporibus nostrum dicta magni commodi laboriosam nobis eaque quasi magnam eveniet perspiciatis optio ullam! Facilis praesentium amet quas quae, deserunt neque eius reprehenderit natus est unde corporis, voluptatibus temporibus eaque at quasi ut vitae odio debitis nostrum repudiandae perferendis eum delectus."
-        ],
-    ];
-
-
-foreach($blog_posts as $post) {
-    if($post["slug"] === $slug) {
-        $new_post = $post;
-    }
-}
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route:: get('posts/{slug}', [postController::class, 'show']);
