0f68e7706bdf5f6a4cc897984eebc137cf227a44
rezadwiyana24 <rezadwiyana93@gmail.com>
2021-10-28 19:18:05 +0700

    Menyelesaikan Vidio 13

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,23 +2,25 @@
 
 @section('container')
 
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                <h2 class="mb-3">{{ $post->title }}</h2>
+                <p>By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
-<div class="container">
-    <div class="row justify-content-center mb-5">
-      <div class="col-md-8">
-  
-        <h1 class="mb-3">{{ $post->title }}</h1>
-  
-        <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-  
-        <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid" alt="{{ $post->category->name }}">
-        <article class="my-3 fs-5">
-          {!! $post->body !!}
-        </article>
-  
-        <a href="/posts" class="d-block mt-3">Back to Posts</a>
-  
-      </div>
+                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+                
+                
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
+                
+
+                <a href="/blog" class="d-block mt-3">Back to Posts</a>
+            </div>
+        </div>
     </div>
-  </div>
+
+
+
 @endsection
